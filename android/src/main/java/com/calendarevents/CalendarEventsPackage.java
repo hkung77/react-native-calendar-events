74da66c94f8d898600612c3ed784e7f3420800f2
hkung77 <henrykung@live.ca>
2018-09-13 18:59:25 +0900

    fix missing js module

--- android/src/main/java/com/calendarevents/CalendarEventsPackage.java
+++ android/src/main/java/com/calendarevents/CalendarEventsPackage.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.bridge.JavaScriptModule;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,8 +18,12 @@ public class CalendarEventsPackage implements ReactPackage {
     }
 
     @Override
-    public List<NativeModule> createNativeModules(
-            ReactApplicationContext reactContext) {
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
 
         modules.add(new CalendarEvents(reactContext));
